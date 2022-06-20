163d55a777778a84883fe639d7719853a0baa4dc
TDarden624 <106000606+TDarden624@users.noreply.github.com>
2022-06-20 10:52:31 -0400

    Update bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,11 +2,16 @@ import time
 import pandas as pd
 import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv', 'Chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv', 'New york city': 'new_york_city.csv',
-              'washington': 'washington.csv', 'Washington': 'washington.csv' }
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
 
 def get_filters():
+    global input_month
+    global input_day
+    
+    month = ['January', 'Febuary', 'March', 'April', 'May', 'June']
+    day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'all']
     """
     Asks user to specify a city, month, and day to analyze.
 
@@ -17,46 +22,47 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     
-    
+    city =['Chicago', 'New York', 'Washington']
     while True:
-        print('\nWhich city would you like to analyze?:')
-        if city.lower() == 'chicago':
-                print("\nAlright, let's check out Chicago!\n")
+        input_city = input('\nWhich city would you like to analyze?:').lower()
+        if input_city == 'Chicago':
+                print("\nAlright, let's check out chicago.lower()!\n")
+                break
                 return 'chicago.csv'
-        elif city.lower() == 'new york':
-                print("\nAlright, let's check out New York!\n")
+        elif input_city == 'New York':
+                print("\nAlright, let's check out New York.lower()!\n")
+                break
                 return 'new_york_city.csv'
-        elif city.lower() == 'washington':
-                print("\nAlright, let's check out Washington!\n")
+        elif input_city == 'Washington':
+                print("\nAlright, let's check out Washington.lower()!\n")
+                break
                 return 'washington.csv'
                 continue
-    while city not in('chicago.csv', 'new_york_city.csv', 'washington.csv'):
-        print("\nPlease enter: Chicago, New York or Washington\n")
-        break
-
-                
+        if input_city not in city:
+           return("\nPlease enter: Chicago, New York or Washington\n")
+           continue
+        else:
+            break
+              
     while True:
-        print("\nPlease choose a month to filter by: January, Febuary, March, April, May, June or all\n")
-        if month not in('January','Febuary','March','April','May','June','none'):
+        input_month = input('\nPlease select a month to filter by or enter none for no filter').lower()
+        print("\nYou've chose {month.title()} as your month.")
+        if input_month not in month:
                 return("\nPlease enter a valid month\n")
                 continue
         else:
-                print("\nYou've chose {month.title()} as your month.")
-        month = input().lower()
-        break
-
-    
+                break
     while True:
-        print("\nPlease choose a day to filter by: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday\n")
-        if day not in('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'all'):
+        input_day = input('\nPlease select a day to filter by or enter all for all options').lower()
+        print("\nPulling up {day.title()}")
+        if input_day not in day:
                 return("\nPlease enter a valid day\n")
+                continue
         else:
-                print("\nPulling up {day.title()}")
-        day = input().lower()
-        break
-    
+            break
+        
     print('-'*40)
-    return city, month, day
+    return input_city, input_month, input_day
 
 
 def load_data(city, month, day):
@@ -173,18 +179,20 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-    print(df)
+        
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+        
+        
+        
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
 
-    time_stats(df)
-    station_stats(df)
-    trip_duration_stats(df)
-    user_stats(df)
-
-    restart = input('\nWould you like to restart? Enter yes or no.\n')
-    if restart.lower() != 'yes':
-    
-
-
-        if __name__ == "__main__":
             
-            main()
+if __name__ == "__main__":
+	main()
+
+       
